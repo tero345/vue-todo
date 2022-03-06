fb407a25d19cb518bceabc58498ca4a5a8c290ce
tero345 <kmaniaen@gmail.com>
2022-03-06 17:44:02 +0900

    todo add

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <header>
+      <h1>Vue todo Typescript</h1>
+    </header>
+    <main>
+      <TodoInput 
+        :item="todoText" 
+        @input="updateTodoText" 
+        @add="addTodoItem"
+      ></TodoInput>
+      <div>
+        <ul>
+          <TodoListItem 
+            v-for="(todoItem, index) in todoItems" 
+            :key="index"
+            :todoItem="todoItem"
+          ></TodoListItem>
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+  import Vue from 'vue';
+  import TodoInput from './components/TodoInput.vue';
+  import TodoListItem from './components/TodoListItem.vue';
 
-export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+  const storage_key = "vue-todo-ts-v1";
+  const storage = {
+    save(todoItems:any[]){
+      const parsed = JSON.stringify(todoItems);
+      localStorage.setItem(storage_key, parsed);
+    },
+    fetch(){
+      const todoItems = localStorage.getItem(storage_key) || "[]";
+      const result = JSON.parse(todoItems);
+      return result;
+    }
+  }
+
+  export default Vue.extend({
+  components: { TodoInput, TodoListItem },
+    data() {
+      return {
+        todoText: "",
+        todoItems: [] as any
+      }
+    },
+    methods:{
+      updateTodoText(value:string){   
+        this.todoText = value;
+      },
+      addTodoItem(){
+        const value = this.todoText;
+        this.todoItems.push(value);
+        storage.save(this.todoItems);
+        this.initTodoText();
+      },
+      initTodoText(){
+        this.todoText = "";
+      },
+      fetchTodoItems(){
+        this.todoItems = storage.fetch();
+      }
+    },
+    created(){
+      this.fetchTodoItems();
+    }
+  });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
